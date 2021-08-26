87177902275e2943ab26a28814ada831e62da26a
undockedseven97 <89567752+undockedseven97@users.noreply.github.com>
2021-08-26 17:56:38 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MES_DEVICE_DISPLAY_ON, function () {
+    led.toggle(2, 4)
 })
+led.toggle(2, 2)
+control.raiseEvent(
+EventBusSource.MES_DPAD_CONTROLLER_ID,
+EventBusValue.MES_DPAD_BUTTON_1_UP
+)
+control.raiseEvent(
+EventBusSource.MES_DPAD_CONTROLLER_ID,
+EventBusValue.MES_REMOTE_CONTROL_EVT_FORWARD
+)
